19b31448437a55c4908e323e3de0903c6fb804b2
SERY2013.ETH <101403669+sery2013@users.noreply.github.com>
2026-03-11 19:20:16 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
-{
-  "name": "kast-2in1",
-  "version": "1.0.0",
-  "type": "module",
-  "scripts": {
-    "dev": "vite",
-    "build": "vite build"
-  },
-  "devDependencies": {
-    "vite": "^5.4.0"
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    outDir: 'dist'
   }
-}
+});
